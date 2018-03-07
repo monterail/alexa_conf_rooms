f60dfe278b268d55824aaadf25fd75badcc5b9d1
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2018-03-07 18:03:48 +0100

    Add room validation

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const Alexa = require('alexa-sdk');
 const moment = require('moment');
 const _ = require('lodash');
 
+const getRoomId = (intent) => {
+  const id = _.get(intent.slots, "room.resolutions.resolutionsPerAuthority.0.values.0.value.id");
+  if (id) {
+    return id;
+  }
+  intent.slots.room = { name: "room" };
+  return null;
+}
+
 const handlers = {
+    'LaunchRequest' : function() {
+        this.emit(':ask', 'What would you like to do?', 'Please say that again?');
+    },
     'FreeConfRooms' : function() {
         this.emit(':tell', "Maybe there are...");
     },
     'RoomBooking' : function() {
         let intent = this.event.request.intent;
         let period = _.get(intent.slots, "period.value");
-        let room = _.get(intent.slots, "room.value");
+        let room = getRoomId(intent);
         console.log(JSON.stringify(intent.slots));
         if (period && room) {
           let periodMinutes = moment.duration(period).asMinutes()
