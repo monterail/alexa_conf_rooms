39a29eb0096e29319227421e69f50f6e17039cee
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2018-03-07 16:03:20 +0100

    First talking version

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
+const moment = require('moment');
+const _ = require('lodash');
 
 const handlers = {
     'FreeConfRooms' : function() {
         this.emit(':tell', "Maybe there are...");
     },
     'RoomBooking' : function() {
-        this.emit(':tell', "Ok, I will...");
+        let intent = this.event.request.intent;
+        let period = _.get(intent.slots, "period.value");
+        let room = _.get(intent.slots, "room.value");
+        console.log(JSON.stringify(intent.slots));
+        if (period && room) {
+          let periodMinutes = moment.duration(period).asMinutes()
+          this.emit(':tell', `Ok, I will book ${room} room for ${periodMinutes} minutes`);
+        } else {
+          this.emit(':delegate', intent);
+        }
     }
 };
 
